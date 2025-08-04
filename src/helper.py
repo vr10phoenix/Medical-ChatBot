00f70a33cd4b083d74f53d6f0679da0e7e6c797c
vr10phoenix <vardhanvachana2005@gmail.com>
2025-08-04 22:31:28 +0530

    Create helper.py
    helper function done

---
+++ src/helper.py
@@ -0,0 +1,23 @@
+from lanchain.document_loaders import PyPDFLoader , DirectoryLoader
+from langchain.text_splitter import RecursiveCharactersTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+
+#Extract data from the PDF
+def load_pdf(data):
+  loader = DirectoryLoader(data , glob = *.pdf , loader_cls = PyPDFLoader)
+  documents = loader.load()
+
+  return documents
+
+#text into chunks 
+def text_split(extracted_data):
+  text_splitter = RecursiveCharacerTextSplitter(chunk_size = 500 , chunk_overlap = 20)
+  text_chunks = text_splitter.split_documents(extracted_data)
+
+  return text_chunks
+
+#download embedding model
+def download_embeddings():
+  embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+  return embeddings
+  
